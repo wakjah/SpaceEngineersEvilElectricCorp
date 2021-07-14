d338416478e7d464ac0e77425b3778308981e9ff
Will Keith <wakjah@gmail.com>
2021-07-14 15:34:24 +0100

    Add deploy of new modinfo stuff and add a special script to deploy the steam content folders for multiplayer testing

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ cp -r src/EvilElectricCorp/Data build
 cp -r src/EvilElectricCorp/Textures build
 cp -r src/EvilElectricCorp/Models build
 cp thumb.png build
+cp metadata.mod build
+cp modinfo.sbmi build
 
 game_mod_folder="$APPDATA/SpaceEngineers/mods/Evil Electric Corp"
 rm -r "$game_mod_folder" || true
